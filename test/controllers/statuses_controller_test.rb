86041bca7dde7fa98e07ef5a623d9d9db85e5cb1
Jameson Beebe <jamesonbeebe@gmail.com>
2015-05-07 20:55:02 -0500

    add status validations and tests

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -23,9 +23,16 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create status" do
+  test "should be logged in to post status" do
+    post :create, status: { content: "hello"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:jameson)
     assert_difference('Status.count') do
-      post :create, status: { content: @status.content}
+    post :create, status: { content: @status.content}
     end
 
     assert_redirected_to status_path(assigns(:status))
@@ -36,21 +43,22 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit when logged in" do
+    sign_in users(:jameson)    
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
+  test "should update status when logged in" do
+    sign_in users(:jameson)
     patch :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
 
   test "should destroy status" do
     assert_difference('Status.count', -1) do
-      delete :destroy, id: @status
-    end
-
+    delete :destroy, id: @status
+  end
     assert_redirected_to statuses_path
   end
 end
--- test/models/status_test.rb
+++ test/models/status_test.rb
@@ -4,4 +4,24 @@ class StatusTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+	test "that a user must add content to post" do
+		status = Status.new
+		assert !status.save
+		assert !status.errors[:content].empty?
+	end
+
+	test "that a comment is at least 2 letters long" do
+		status = Status.new
+		status.content = "H"
+		assert !status.save
+		assert !status.errors[:content].empty?
+	end
+	
+	test "that a comment must have a user" do
+		status = Status.new
+		status.content = "Hello"
+		assert !status.save
+		assert !status.errors[:user_id].empty?
+	end
+
 end
