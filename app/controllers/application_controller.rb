f329703e76558ad9e2c9c3a8ef62b4b7cb5d2615
Jameson Beebe <jamesonbeebe@gmail.com>
2015-05-07 19:44:23 -0500

    added helper in application_helper.rb for flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
-
+	helper ApplicationHelper
 	protected
 		def configure_permitted_parameters
 			devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name << :profile_name << :email << :password << :password_confirmation
